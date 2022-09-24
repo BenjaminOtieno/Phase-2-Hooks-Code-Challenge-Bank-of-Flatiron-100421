bc1a680ae57049f3ca09bcd14f9816276d6541a4
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-09-24 20:33:54 +0300

    uno

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Search() {
+const Search = (props) => {
+  let handleChange = (event) => {
+    props.searchFun(event.target.value)
+  }
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
-        placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        placeholder={"Search your Recent Transactions"}
+        value={props.searchValue}
+        onChange={handleChange}
       />
       <i className="circular search link icon"></i>
     </div>
